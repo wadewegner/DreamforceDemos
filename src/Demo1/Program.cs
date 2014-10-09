195a2552df9cd744492ea30217ef0fd3faed300b
Wade Wegner <wade.wegner@gmail.com>
2014-10-09 14:23:07 -0700

    Done with demo 1

--- src/Demo1/Program.cs
+++ src/Demo1/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Salesforce.Common;
+using Salesforce.Force;
 
 namespace Demo1
 {
     class Program
     {
+        private static string _securityToken = "";
+        private static string _clientId = "";
+        private static string _clientSecret = "";
+        private static string _username = "";
+        private static string _password = "" + _securityToken;
+
         static void Main(string[] args)
         {
+            var auth = new AuthenticationClient();
+            auth.UsernamePasswordAsync(_clientId, _clientSecret, _username, _password).Wait();
+
+            var client = new ForceClient(auth.InstanceUrl, auth.AccessToken, auth.ApiVersion);
+
+            var results = client.QueryAsync<dynamic>("SELECT Id, Name, Description FROM Account");
+            results.Wait();
+
+            var accounts = results.Result.records;
+
+            Console.WriteLine(accounts.Count);
         }
     }
 }
